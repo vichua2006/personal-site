d87c155464034f7fd6dc3e08517580f36e4a7f6d
Victor Huang <vichua2006@gmail.com>
2025-05-31 13:53:59 -0400

    chore: added site traffic monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/next';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -6,5 +7,6 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
